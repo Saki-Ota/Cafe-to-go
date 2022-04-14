b36d663dce9256d8f941e3f07de713728dcf32b5
Gregory Koutsantonis <gregkoutsa@gmail.com>
2022-04-14 20:53:19 +1000

    added devise for users and some controller methods for cafes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all # Need to Change to only include products from this cafe
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+  def product_params
+    params.require(:product).permit(:category, :description, :name, :hidden, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users
+  # resources :users
 
   resources :carts, only: %i[show] do
     resources :items, only: %i[destroy]
   end
 
-  resources :cafes, only: %i[index show new create destroy update] do
+  resources :cafes do
     resources :products, only: %i[index show new create] do
       resources :carts, only: %i[new create]
       resources :items, only: %i[new create]
