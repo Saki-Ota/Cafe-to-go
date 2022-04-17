238155a2b2bb7744a6a1a173bee0fd0cae1b4085
Gregory Koutsantonis <gregkoutsa@gmail.com>
2022-04-18 02:39:18 +1000

    adding items to cart in cafe menu page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,15 +1,6 @@
 class CartsController < ApplicationController
-  def new
-    if Cart.find_by(user_id: current_user_id).last.active == false
-      @cart = Cart.new(user_id: current_user_id)
-    end
-  end
+  def checkout
 
-  def create
-    if Cart.find_by(user_id: current_user_id).last.active == false
-      @cart = Cart.new(user_id: current_user_id)
-      @cart.save
-    end
   end
 
   def show
