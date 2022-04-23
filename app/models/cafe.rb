c855054d5925de4e1c654454bbd2f761c1175fe8
Gregory Koutsantonis <gregkoutsa@gmail.com>
2022-04-23 15:41:16 +1000

    ability to delete a cafe

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,6 +1,6 @@
 class Cafe < ApplicationRecord
   belongs_to :user
-  has_many :product, dependent: :destroy
+  has_many :products, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   validates :address, :name, :description, presence: true
